447349a4ce0cca3d9dcc920d220570f9ddd10b55
Jonathan Bennett <jbennett@ccistudios.com>
2011-02-19 21:32:56 -0500

    Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompanyManager::Application.config.secret_token = 'c7d3e011d51764081ab8cf4b4b94ead638ea8eb3b46d8c9dbb59342e48a32f4e5b6f1934d340593e5c9e058dd1e41b0047117164529b4a5ab68a2e9e4d823e95'
