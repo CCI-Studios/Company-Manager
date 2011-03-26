c7616497a8942548a7a899b1d578ffb6ec567722
Jonathan Bennett <jbennett@ccistudios.com>
2011-03-26 12:42:33 -0400

    Added email to client

--- app/models/client.rb
+++ app/models/client.rb
@@ -22,8 +22,8 @@
 #
 
 class Client < ActiveRecord::Base
-	attr_accessible :first_name, :last_name, :title, :phone1, :extension1, :phone2, :extension2, :address1, :address2, :city, :province, :country, :postal_code
-	
+	attr_accessible :first_name, :last_name, :title, :phone1, :extension1, :phone2, :extension2, :email, :address1, :address2, :city, :province, :country, :postal_code
+
 	validates :first_name, :presence => true
 	# validates :last_name, :presence => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110219231503) do
+ActiveRecord::Schema.define(:version => 20110326155742) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
