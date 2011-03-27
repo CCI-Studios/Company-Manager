c5f031fa0aeca79ad53b7e80004c437c42f67179
Jonathan Bennett <jbennett@ccistudios.com>
2011-03-27 02:37:01 -0400

    Begin testing of client model

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,7 +1,38 @@
 require 'spec_helper'
 
 describe Client do
-  pending "add some examples to (or delete) #{__FILE__}"
 
-	it "should require first name"
+  before(:each) do
+    @attr = { :first_name => 'Jim', :last_name => 'Smith', :title => 'Mr.',
+      :phone1 => '5195551234', :extension1 => '1234', :phone2 => '5193455555', :extension2 => '2345',
+      :address1 => '123 Here Street', :address2 => 'Unit 3', :city => 'Sarnia', :province => 'On', :country => 'Canada', :postal_code => 'j2j 4j5' }
+  end
+
+  it "should create new client given valid attributes" do
+      Client.create!(@attr);
+  end
+
+	it "should require first name" do
+    c = Client.new(@attr.merge(:first_name => ""))
+    c.should_not be_valid
+  end
+
+	it "should require last name" do
+	  c = Client.new(@attr.merge(:last_name =>""))
+	  c.should_not be_valid
+  end
+
+	it "should require phone number" do
+	  c = Client.new(@attr.merge(:phone1 => ""))
+  	c.should_not be_valid
+  end
+
+	it "should have valid fullname" do
+	  c = Client.new(@attr)
+	  fullname = "#{c.last_name}, #{c.first_name}"
+	  c.fullname.should == fullname
+	end
+
+
+	it "should have properly formated phone number"
 end
